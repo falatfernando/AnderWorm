b8a527e0de3c8244c6d74d640872e8d5005dad42
Fernando Falat <fernandofalat@protonmail.com>
2023-10-08 16:56:11 -0300

    Update csv_converter.py

--- aux_files/csv_converter.py
+++ aux_files/csv_converter.py
@@ -11,3 +11,15 @@ def convert_pair_gz_to_csv(folder_path):
 
 convert_pair_gz_to_csv(r'C:\Users\ferna\Documents\GitHub\anderworm\Project\ds_shenzou\normalized')
 print('done')
+
+import chardet
+
+def detect_encoding(file_path):
+    with open(file_path, 'rb') as file:
+        result = chardet.detect(file.read())
+        return result['encoding']
+
+# Example usage:
+file_path = r'C:\Users\ferna\Documents\GitHub\anderworm\Project\ds_shenzou\normalized\GLDS-167_microarray_GSM2412681_1A_532_norm_RMA.csv'
+encoding = detect_encoding(file_path)
+print(f"The encoding of the file is: {encoding}")
